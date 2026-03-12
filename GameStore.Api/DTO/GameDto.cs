8ad002a96d9877064f4016495988a8fe68d8c891
Art'm <kasko.artem45@gmail.com>
2026-03-12 23:03:02 +0300

    Create GameDto.cs

---
+++ GameStore.Api/DTO/GameDto.cs
@@ -0,0 +1,15 @@
+namespace GameStore.Api.DTO;
+
+/// <summary>
+/// A DTO is a contract between the client and server
+/// since it represents a shared agreement about how
+/// data will be transferred and used.
+/// </summary>
+public record GameDto(
+    int Id,
+    string Name,
+    string Genre,
+    decimal Price,
+    DateOnly ReleaseDate
+    );
+    
