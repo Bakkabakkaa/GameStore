799577db6a84c76692da533b559d2755fd5a64aa
Art'm <kasko.artem45@gmail.com>
2026-03-12 23:13:22 +0300

    Add mock list of games and add endpoint /games

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,16 @@
+using GameStore.Api.DTO;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<GameDto> games = new List<GameDto>()
+{
+    new GameDto(1, "Street Fighter 2", "Fighting", 19.99M, new DateOnly(1992, 7, 15)),
+    new GameDto(2, "Final Fantasy 7 Rebirth", "RPG", 69.99M, new DateOnly(2024, 2, 29)),
+    new GameDto(3, "Astro Bot", "Platformer", 59.99M, new DateOnly(2024, 9, 6))
+};
+
+// GET /games
+app.MapGet("/games", () => games);
 
 app.Run();
