28c1af34ce4b58d79c7e541c5a6e960274a86282
Art'm <kasko.artem45@gmail.com>
2026-03-14 16:44:49 +0300

    Configure SQLite database and register GameStoreContext

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,8 +1,11 @@
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 
+var connString = "Data Source=GameStore.db";
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidation();
+builder.Services.AddSqlite<GameStoreContext>(connString);
 
 var app = builder.Build();
 
